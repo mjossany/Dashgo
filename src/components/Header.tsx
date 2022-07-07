d8dbcebff306a74c1bb2494fafb26fdb6dca401c
Jossany Moura <mjossany@gmail.com>
2022-07-07 10:53:04 -0300

    Complete Header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,5 @@
-import { Flex, Icon, Input, Text } from "@chakra-ui/react";
-import { RiSearchLine } from 'react-icons/ri';
+import { Avatar, Box, Flex, HStack, Icon, Input, Text } from "@chakra-ui/react";
+import { RiNotificationLine, RiSearchLine, RiUserAddLine } from 'react-icons/ri';
 
 export function Header() {
   return (
@@ -54,6 +54,40 @@ export function Header() {
           as={RiSearchLine}
           fontSize="20"
         />
+      </Flex>
+
+      <Flex
+        align="center"
+        ml="auto"
+      >
+        <HStack
+          spacing="8"
+          mx="8"
+          pr="8"
+          py="1"
+          color="gray.300"
+          borderRightWidth={1}
+          borderColor="gray.700"
+        >
+          <Icon as={RiNotificationLine} fontSize="20" />
+          <Icon as={RiUserAddLine} fontSize="20" />
+        </HStack>
+
+        <Flex align="center">
+          <Box
+            mr="4"
+            textAlign="right"
+          >
+            <Text>Jossany Moura</Text>
+            <Text
+              color="gray.300"
+              fontSize="small"
+            >
+              mjossany@gmail.com
+            </Text>
+          </Box>
+          <Avatar size="md" name="Jossany Moura" src="https://github.com/mjossany.png"/>
+        </Flex>
       </Flex>
     </Flex>
   );
