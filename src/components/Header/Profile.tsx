8dd6b8da031cba38a98c202c623b000c43846030
Jossany Moura <mjossany@gmail.com>
2022-07-11 15:58:25 -0300

    Implement responsiveness on Header components

--- src/components/Header/Profile.tsx
+++ src/components/Header/Profile.tsx
@@ -1,25 +1,21 @@
 import { Avatar, Box, Flex, Text } from "@chakra-ui/react";
 
-export function Profile() {
+interface ProfileProps {
+  showProfileData: boolean;
+}
+
+export function Profile({ showProfileData }: ProfileProps) {
   return (
     <Flex align="center">
-      <Box
-        mr="4"
-        textAlign="right"
-      >
+      { showProfileData && (
+        <Box mr="4" textAlign="right">
         <Text>Jossany Moura</Text>
-        <Text
-          color="gray.300"
-          fontSize="small"
-        >
+        <Text color="gray.300" fontSize="small">
           mjossany@gmail.com
         </Text>
       </Box>
-      <Avatar
-        size="md"
-        name="Jossany Moura"
-        src="https://github.com/mjossany.png"
-      />
+      )}
+      <Avatar size="md" name="Jossany Moura" src="https://github.com/mjossany.png"/>
     </Flex>
   );
 }
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,4 +1,4 @@
-import { Flex } from "@chakra-ui/react";
+import { Flex, useBreakpointValue } from "@chakra-ui/react";
 
 import { Logo } from "./Logo";
 import { NotificationsNav } from "./NotificationsNav";
@@ -6,13 +6,17 @@ import { Profile } from "./Profile";
 import { SearchBox } from "./SearchBox";
 
 export function Header() {
+  const isWideVersion = useBreakpointValue({
+    base: false,
+    lg: true,
+  })
   return (
     <Flex as="header" w="100%" maxWidth={1480} h="20" marginX="auto" mt="4" px="6" align="center">
       <Logo />
-      <SearchBox />
+      { isWideVersion && <SearchBox /> }
       <Flex align="center" ml="auto">
         <NotificationsNav />
-        <Profile />
+        <Profile showProfileData={isWideVersion} />
       </Flex>
     </Flex>
   );
