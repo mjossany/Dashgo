388bb1ef639c3857f9ac11af386d4c7398170480
Jossany Moura <mjossany@gmail.com>
2022-07-12 10:38:48 -0300

    Implement responsiveness on UserList and Pagination

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,10 +1,15 @@
-import { Box, Button, Checkbox, Flex, Heading, Icon, Table, Tbody, Td, Text, Th, Thead, Tr } from "@chakra-ui/react";
+import { Box, Button, Checkbox, Flex, Heading, Icon, Table, Tbody, Td, Text, Th, Thead, Tr, useBreakpointValue } from "@chakra-ui/react";
 import { RiAddLine, RiPencilLine } from "react-icons/ri";
 import { Header } from "../../components/Header";
 import { Pagination } from "../../components/Pagination";
 import { Sidebar } from "../../components/Sidebar";
 
 export default function UserList() {
+  const isWideVersion = useBreakpointValue({
+    base: false,
+    lg: true,
+  });
+
   return (
     <Box>
       <Header />
@@ -51,15 +56,13 @@ export default function UserList() {
           >
             <Thead>
               <Tr>
-                <Th px="6" color="gray.300" width="8">
+                <Th px={["4", "4", "6"]} color="gray.300" width="8">
                   <Checkbox colorScheme="pink" />
                 </Th>
                 <Th>
                   User
                 </Th>
-                <Th>
-                  Subscription date
-                </Th>
+                { isWideVersion && <Th>Subscription Date</Th>}
                 <Th w="8">
 
                 </Th>
@@ -67,7 +70,7 @@ export default function UserList() {
             </Thead>
             <Tbody>
               <Tr>
-                <Td px="6">
+                <Td px={["4", "4", "6"]}>
                   <Checkbox colorScheme="pink" />
                 </Td>
                 <Td>
@@ -85,23 +88,12 @@ export default function UserList() {
                     </Text>
                   </Box>
                 </Td>
-                <Td>
+                { isWideVersion && <Td>
                   July 8, 2022
-                </Td>
-                <Td>
-                  <Button
-                    as="a"
-                    size="sm"
-                    fontSize="sm"
-                    colorScheme="purple"
-                    leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
-                  >
-                    Edit
-                  </Button>
-                </Td>
+                </Td> }
               </Tr>
               <Tr>
-                <Td px="6">
+                <Td px={["4", "4", "6"]}>
                   <Checkbox colorScheme="pink" />
                 </Td>
                 <Td>
@@ -119,23 +111,12 @@ export default function UserList() {
                     </Text>
                   </Box>
                 </Td>
-                <Td>
+                { isWideVersion && <Td>
                   July 8, 2022
-                </Td>
-                <Td>
-                  <Button
-                    as="a"
-                    size="sm"
-                    fontSize="sm"
-                    colorScheme="purple"
-                    leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
-                  >
-                    Edit
-                  </Button>
-                </Td>
+                </Td> }
               </Tr>
               <Tr>
-                <Td px="6">
+                <Td px={["4", "4", "6"]}>
                   <Checkbox colorScheme="pink" />
                 </Td>
                 <Td>
@@ -153,24 +134,12 @@ export default function UserList() {
                     </Text>
                   </Box>
                 </Td>
-                <Td>
+                { isWideVersion && <Td>
                   July 8, 2022
-                </Td>
-                <Td>
-                  <Button
-                    as="a"
-                    size="sm"
-                    fontSize="sm"
-                    colorScheme="purple"
-                    leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
-                  >
-                    Edit
-                  </Button>
-                </Td>
+                </Td> }
               </Tr>
             </Tbody>
           </Table>
-
           <Pagination />
         </Box>
       </Flex>
